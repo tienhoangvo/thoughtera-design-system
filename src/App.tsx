801040bd587e333a88865751bd9f2ed76b7921bc
Tien Hoang Vo <tien.vo@dmchosting.ca>
2023-02-15 17:54:49 +0700

    convert hex color codes to hsl parameters

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,6 @@
-import { useRef } from 'react'
+import { ReactElement, useRef } from 'react'
 import './App.css'
-import Button from './components/Button'
-import Icon from './components/Icon'
+import Button, { ButtonProps } from './components/Button'
 import Table from './components/Table'
 import TBody from './components/Table/TBody'
 import TBodyCell from './components/Table/TBodyCell'
@@ -19,222 +18,52 @@ function App() {
         <TCaption>Thoughtera components</TCaption>
         <THead>
           <TRow>
-            <THeaderCell colSpan={2}>Components</THeaderCell>
-            <THeaderCell>Variants</THeaderCell>
-            <THeaderCell>States</THeaderCell>
+            <THeaderCell>Component</THeaderCell>
+            <THeaderCell>Variant</THeaderCell>
+            <THeaderCell>UI</THeaderCell>
           </TRow>
-          <TRow>
+          {/* <TRow>
             <THeaderCell>Name</THeaderCell>
             <THeaderCell>UI</THeaderCell>
             <THeaderCell>Variants</THeaderCell>
-            <THeaderCell>States</THeaderCell>
-          </TRow>
+          </TRow> */}
         </THead>
         <TBody>
-          <TRow>
-            <TBodyCell>Button</TBodyCell>
-            <TBodyCell>
-              <Button variant="filled">Button</Button>
-            </TBodyCell>
-            <TBodyCell>
-              <ul>
-                <li>
-                  <input type="radio" name="variant" value="filled" />
-                  <span>Filled</span>
-                </li>
-                <li>
-                  <input type="radio" name="variant" value="outlined" />
-                  <span>Outlined</span>
-                </li>
-                <li>
-                  <input type="radio" name="variant" value="text" />
-                  <span>Text</span>
-                </li>
-                <li>
-                  <input type="radio" name="variant" value="filled-tonal" />
-                  <span>Filled-tonal</span>
-                </li>
-                <li>
-                  <input type="radio" name="variant" value="elevated" />
-                  <span>Elevated</span>
-                </li>
-              </ul>
-            </TBodyCell>
-            <TBodyCell>
-              <ul>
-                <li>
-                  <input type="radio" name="state" value="hover" />
-                  <span>hover</span>
-                </li>
-                <li>
-                  <input type="radio" name="state" value="focus" />
-                  <span>focus</span>
-                </li>
-                <li>
-                  <input type="radio" name="state" value="pressed" />
-                  <span>pressed</span>
-                </li>
-                <li>
-                  <input type="radio" name="state" value="disabled" />
-                  <span>disabled</span>
-                </li>
-              </ul>
-            </TBodyCell>
-          </TRow>
-          <TRow>
-            <TBodyCell>Button</TBodyCell>
-            <TBodyCell>
-              <Button variant="outlined">Outlined</Button>
-            </TBodyCell>
-            <TBodyCell>
-              <ul>
-                <li>
-                  <input type="radio" name="variant" value="filled" />
-                  <span>Filled</span>
-                </li>
-                <li>
-                  <input type="radio" name="variant" value="outlined" />
-                  <span>Outlined</span>
-                </li>
-                <li>
-                  <input type="radio" name="variant" value="text" />
-                  <span>Text</span>
-                </li>
-                <li>
-                  <input type="radio" name="variant" value="filled-tonal" />
-                  <span>Filled-tonal</span>
-                </li>
-                <li>
-                  <input type="radio" name="variant" value="elevated" />
-                  <span>Elevated</span>
-                </li>
-              </ul>
-            </TBodyCell>
-            <TBodyCell>
-              <ul>
-                <li>
-                  <input type="radio" name="state" value="hover" />
-                  <span>hover</span>
-                </li>
-                <li>
-                  <input type="radio" name="state" value="focus" />
-                  <span>focus</span>
-                </li>
-                <li>
-                  <input type="radio" name="state" value="pressed" />
-                  <span>pressed</span>
-                </li>
-                <li>
-                  <input type="radio" name="state" value="disabled" />
-                  <span>disabled</span>
-                </li>
-              </ul>
-            </TBodyCell>
-          </TRow>
-          <TRow>
-            <TBodyCell>Button</TBodyCell>
-            <TBodyCell>
-              <Button variant="text">Text</Button>
-            </TBodyCell>
-            <TBodyCell>
-              <ul>
-                <li>
-                  <input type="radio" name="variant" value="filled" />
-                  <span>Filled</span>
-                </li>
-                <li>
-                  <input type="radio" name="variant" value="outlined" />
-                  <span>Outlined</span>
-                </li>
-                <li>
-                  <input type="radio" name="variant" value="text" />
-                  <span>Text</span>
-                </li>
-                <li>
-                  <input type="radio" name="variant" value="filled-tonal" />
-                  <span>Filled-tonal</span>
-                </li>
-                <li>
-                  <input type="radio" name="variant" value="elevated" />
-                  <span>Elevated</span>
-                </li>
-              </ul>
-            </TBodyCell>
-            <TBodyCell>
-              <ul>
-                <li>
-                  <input type="radio" name="state" value="hover" />
-                  <span>hover</span>
-                </li>
-                <li>
-                  <input type="radio" name="state" value="focus" />
-                  <span>focus</span>
-                </li>
-                <li>
-                  <input type="radio" name="state" value="pressed" />
-                  <span>pressed</span>
-                </li>
-                <li>
-                  <input type="radio" name="state" value="disabled" />
-                  <span>disabled</span>
-                </li>
-              </ul>
-            </TBodyCell>
-          </TRow>
-          <TRow>
-            <TBodyCell>Button</TBodyCell>
-            <TBodyCell>
-              <Button variant="filled-tonal">Filled-tonal</Button>
-            </TBodyCell>
-            <TBodyCell>
-              <ul>
-                <li>
-                  <input type="radio" name="variant" value="filled" />
-                  <span>Filled</span>
-                </li>
-                <li>
-                  <input type="radio" name="variant" value="outlined" />
-                  <span>Outlined</span>
-                </li>
-                <li>
-                  <input type="radio" name="variant" value="text" />
-                  <span>Text</span>
-                </li>
-                <li>
-                  <input type="radio" name="variant" value="filled-tonal" />
-                  <span>Filled-tonal</span>
-                </li>
-                <li>
-                  <input type="radio" name="variant" value="elevated" />
-                  <span>Elevated</span>
-                </li>
-              </ul>
-            </TBodyCell>
-            <TBodyCell>
-              <ul>
-                <li>
-                  <input type="radio" name="state" value="hover" />
-                  <span>hover</span>
-                </li>
-                <li>
-                  <input type="radio" name="state" value="focus" />
-                  <span>focus</span>
-                </li>
-                <li>
-                  <input type="radio" name="state" value="pressed" />
-                  <span>pressed</span>
-                </li>
-                <li>
-                  <input type="radio" name="state" value="disabled" />
-                  <span>disabled</span>
-                </li>
-              </ul>
-            </TBodyCell>
-          </TRow>
+          {
+            components.map((c) => (
+              <>
+              {c.variants.map((variant, index, list) => (
+                <TRow>
+                  {index === 0 && <THeaderCell rowSpan={list.length}>{c.name}</THeaderCell>}
+                  <TBodyCell>
+                    {variant}
+                  </TBodyCell>
+                  <TBodyCell>
+                    {c.renderComponent({ variant, children: variant })}
+                  </TBodyCell>
+              </TRow>
+              ))}
+              </>
+            ))
+          }
         </TBody>
       </Table>
     </div>
   )
 }
+
+type ComponentType = {
+  name: string,
+  variants: ButtonProps['variant'][],
+  renderComponent: (props:any) => ReactElement,
+}
+
+const components: ComponentType[] = [
+  {
+    name: 'Button',
+    variants: ['filled', 'outlined', 'text', 'filled-tonal', 'elevated'],
+    renderComponent: (props: ButtonProps) => <Button {...props}>{props.children}</Button>
+  }
+]
 
 export default App
