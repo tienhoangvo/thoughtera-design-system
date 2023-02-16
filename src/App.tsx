5cf1cf62b5b47a76cf943fba054db857bd13bed5
Tien Hoang Vo <tien.vo@dmchosting.ca>
2023-02-16 17:06:55 +0700

    uodate state tokens

--- src/App.tsx
+++ src/App.tsx
@@ -10,23 +10,20 @@ import THeaderCell from './components/Table/THeaderCell'
 import TRow from './components/Table/TRow'
 
 function App() {
-  const ref = useRef(null)
-
   return (
     <div className="App">
       <Table>
         <TCaption>Thoughtera components</TCaption>
         <THead>
           <TRow>
-            <THeaderCell>Component</THeaderCell>
-            <THeaderCell>Variant</THeaderCell>
-            <THeaderCell>UI</THeaderCell>
+            <THeaderCell rowSpan={2}>Component</THeaderCell>
+            <THeaderCell rowSpan={2}>Variant</THeaderCell>
+            <THeaderCell colSpan={2}>UI</THeaderCell>
           </TRow>
-          {/* <TRow>
-            <THeaderCell>Name</THeaderCell>
-            <THeaderCell>UI</THeaderCell>
-            <THeaderCell>Variants</THeaderCell>
-          </TRow> */}
+          <TRow>
+            <THeaderCell>States</THeaderCell>
+            <THeaderCell>Disabled</THeaderCell>
+          </TRow>
         </THead>
         <TBody>
           {components.map((c) => (
@@ -36,6 +33,7 @@ function App() {
                   {index === 0 && <THeaderCell rowSpan={list.length}>{c.name}</THeaderCell>}
                   <TBodyCell>{variant}</TBodyCell>
                   <TBodyCell>{c.renderComponent({ variant, children: variant })}</TBodyCell>
+                  <TBodyCell>{c.renderComponent({ variant, children: variant, disabled: true })}</TBodyCell>
                 </TRow>
               ))}
             </Fragment>
--- src/components/Table/TBodyCell/TBodyCell.tsx
+++ src/components/Table/TBodyCell/TBodyCell.tsx
@@ -1,13 +1,19 @@
-import { forwardRef, HTMLAttributes, ReactNode } from "react";
+import clsx from "clsx";
+import { forwardRef, HTMLAttributes, ReactNode, useMemo } from "react";
 import styles from './TBodyCell.module.css'
 
 export type TBodyCellProps = HTMLAttributes<HTMLTableCellElement> & {
   children: ReactNode
 }
 
-const TBodyCell = forwardRef<HTMLTableCellElement, TBodyCellProps>(({children}) => {
+const TBodyCell = forwardRef<HTMLTableCellElement, TBodyCellProps>(({ className, children }, ref) => {
+
+  const classList = useMemo(() => {
+    return className ? clsx(styles.tbodycell, className) : styles.tbodycell
+  }, [className])
+
   return (
-    <td className={styles.tbodycell}>{children}</td>
+    <td className={classList} ref={ref}>{children}</td>
     )
 })
 
